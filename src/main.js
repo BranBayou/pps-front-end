0166e5f5cbb967c9f6876f8f3f2c6684cb97997f
Berhanu Baye <berhan.baye@gmail.com>
2025-11-28 16:25:26 +0300

    configure pinia and axios globally

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import './assets/main.css'
-
-
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
-import App from './App.vue'
+import App from './App.vue';
+import api from './plugins/axios';
+
 
 const app = createApp(App);
+const pinia = createPinia();
 app.use(PrimeVue);
-
+app.use(pinia);
+app.config.globalProperties.$api = api;
+app.provide('api', api);
 app.mount('#app');
